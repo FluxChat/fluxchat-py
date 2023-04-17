c889af544d209d5b8d07f786e7e1dd79f58ea21b
Christian Mayer <christian@fox21.at>
2023-04-17 14:42:31 +0200

    Wait for other process to create PID file.

--- bin/start.sh
+++ bin/start.sh
@@ -5,6 +5,7 @@ cd "${SCRIPT_BASEDIR}/.."
 
 ./bin/pychat.sh 1> /dev/null 2> /dev/null < /dev/null &
 if test $? -eq 0 ; then
+	sleep 0.2
 	pid_file=var/data1/pychat.pid
 	pid=$(cat ${pid_file})
 	echo "-> started: ${pid}"
