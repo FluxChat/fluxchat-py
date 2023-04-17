2ab5a36bd84635caf218b1ba06572950848ac182
Christian Mayer <christian@fox21.at>
2023-04-17 14:37:17 +0200

    No extra log file is needed.

--- bin/start.sh
+++ bin/start.sh
@@ -3,7 +3,7 @@
 SCRIPT_BASEDIR=$(dirname "$0")
 cd "${SCRIPT_BASEDIR}/.."
 
-./bin/pychat.sh 1>> var/data1/stdout.log 2>&1 < /dev/null &
+./bin/pychat.sh 1> /dev/null 2> /dev/null < /dev/null &
 if test $? -eq 0 ; then
 	pid_file=var/data1/pychat.pid
 	pid=$(cat ${pid_file})
