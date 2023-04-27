d7c408092cc4a7349e7f10d20d1a5054adbf18f4
Christian Mayer <christian@fox21.at>
2023-04-27 20:42:32 +0200

    Tests

--- src/tests/test_node.py
+++ src/tests/test_node.py
@@ -4,10 +4,10 @@ from lib.overlay import Node, Distance
 
 class NodeTestCase(unittest.TestCase):
 	def setUp(self):
-		self.node1 = Node('FC_2QVEiC82QXQVg97AAHmjiMsmnNjN')
+		self.node1 = Node('FC_d7vkj3XKCF8JNpt3MMacwhcMfz75tJHEF2Zou4m4pih')
 
 	def test_str(self):
-		self.assertEqual(str(self.node1), 'Node(FC_2QVEiC82QXQVg97AAHmjiMsmnNjN)')
+		self.assertEqual(str(self.node1), 'Node(FC_d7vkj3XKCF8JNpt3MMacwhcMfz75tJHEF2Zou4m4pih)')
 
 	def test_eq1(self):
 		self.assertEqual(Node('FC_123456788'), Node('FC_123456788'))
@@ -28,6 +28,6 @@ class NodeTestCase(unittest.TestCase):
 		self.assertFalse(Node('XYZ').has_valid_id())
 
 	def test_distance(self):
-		node2 = Node('FC_2QVEiC82QXQVg97AAHmjiMsmnNjN')
+		node2 = Node('FC_d7vkj3XKCF8JNpt3MMacwhcMfz75tJHEF2Zou4m4pih')
 		distance = self.node1.distance(node2)
 		self.assertEqual(type(distance), Distance)
