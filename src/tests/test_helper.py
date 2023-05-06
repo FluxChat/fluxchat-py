b6db528dc95b146e7bf9f36ab03ca13502a7fcdd
Christian Mayer <christian@fox21.at>
2023-05-06 10:41:25 +0200

    Tests

--- src/tests/test_helper.py
+++ src/tests/test_helper.py
@@ -1,6 +1,6 @@
 
 import unittest
-from lib.helper import generate_id_from_public_key_file, resolve_contact, is_valid_uuid
+from lib.helper import generate_id_from_public_key_file, resolve_contact, is_valid_uuid, binary_encode, binary_decode
 
 class HelperTestCase(unittest.TestCase):
 	def test_generate_id_from_public_key_file(self):
@@ -34,3 +34,30 @@ class HelperTestCase(unittest.TestCase):
 	def test_is_valid_uuid_v4(self):
 		self.assertTrue(is_valid_uuid('642b8d3b-7b73-4941-bb11-c6ac13c18ba5'))
 		self.assertFalse(is_valid_uuid('xyz'))
+
+	def test_binary_encode(self):
+		data = {
+			0x01: 'ABC',
+			0x02: 'DEF',
+		}
+		encoded = binary_encode(data, 1)
+		self.assertEqual(encoded, b'\x01\x03ABC\x02\x03DEF')
+
+		data = {
+			0x01: 'ABC',
+			0x02: b'DEF',
+		}
+		encoded = binary_encode(data, 1)
+		self.assertEqual(encoded, b'\x01\x03ABC\x02\x03DEF')
+
+
+		data = {
+			0x01: 'ABC',
+			0x02: 'DEF',
+		}
+		encoded = binary_encode(data, 4)
+		self.assertEqual(encoded, b'\x01\x03\x00\x00\x00ABC\x02\x03\x00\x00\x00DEF')
+
+
+		with self.assertRaises(AttributeError) as context:
+			binary_encode({'A': 'ABC'}, 1)
--- src/tests/test_task.py
+++ src/tests/test_task.py
@@ -1,7 +1,27 @@
 
 import unittest
+import datetime as dt
+import time
+
 from lib.task import Task
 
-class TaskTestCase(unittest.TestCase): # TODO: add more tests
+class TaskTestCase(unittest.TestCase):
 	def test_task1(self):
-		task = Task(lambda x: x)
+		task = Task(lambda: True)
+		self.assertTrue(task.run())
+
+	def test_task2(self):
+		task = Task(lambda: 'ABC', dt.timedelta(minutes=1))
+		self.assertEqual(task.run(), 'ABC')
+		self.assertEqual(task.run(), False)
+
+	def test_task3(self):
+		task = Task(lambda: 'ABC', dt.timedelta(minutes=1), True)
+		self.assertEqual(task.run(), False)
+		self.assertEqual(task.run(), False)
+
+	def test_task4(self):
+		task = Task(lambda: 'ABC', dt.timedelta(milliseconds=100))
+		self.assertEqual(task.run(), 'ABC')
+		time.sleep(0.2)
+		self.assertEqual(task.run(), 'ABC')
